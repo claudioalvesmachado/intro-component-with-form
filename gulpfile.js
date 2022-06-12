53560a1e07982a839095802ab5d8b747d5593d2d
Cláudio <claudiodeyvidalves@gmail.com>
2022-06-12 18:51:50 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const uglify = require('gulp-uglify');
+
+function buildStyles() {
+  return gulp.src('./src/scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        overrideBrowserslist: ['last 2 versions'],
+        cascade: false
+      }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('./public/css/'));
+};
+
+
+
+function compressJs() {
+    return gulp.src('./src/js/main.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js/'))
+}
+
+
+
+exports.buildStyles = buildStyles;
+exports.compressJs = compressJs
+exports.watch = function () {
+  gulp.watch('./src/scss/', gulp.series('buildStyles'));
+  gulp.watch('./src/js/', gulp.series('compressJs'))
+};
